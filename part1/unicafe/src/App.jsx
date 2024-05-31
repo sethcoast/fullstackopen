905f30f5428f1edbdd96e80e80778a7e94b97e1d
Seth Donaldson <sethd63@gmail.com>
2024-05-31 13:32:50 -0400

    1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -13,20 +13,34 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [avg, setAvg] = useState(0)
+  const [posPercent, setPosPercent] = useState(0)
 
   const handleGood = () => {
     const newGood = good + 1
     setGood(newGood)
+    const newAvg = (newGood - bad)/(newGood + bad + neutral)
+    setAvg(newAvg)
+    const newPosPerc = newGood / (newGood + bad + neutral)
+    setPosPercent(newPosPerc)
   }
 
   const handleNeutral = () => {
     const newNeutral = neutral + 1
     setNeutral(newNeutral)
+    const newAvg = (good - bad)/(good + bad + newNeutral)
+    setAvg(newAvg)
+    const newPosPerc = good / (good + bad + newNeutral)
+    setPosPercent(newPosPerc)
   }
 
   const handleBad = () => {
     const newBad = bad + 1
     setBad(newBad)
+    const newAvg = (good - newBad)/(good + newBad + neutral)
+    setAvg(newAvg)
+    const newPosPerc = good / (good + newBad + neutral)
+    setPosPercent(newPosPerc)
   }
 
   return (
@@ -39,6 +53,8 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>average {avg}</p>
+      <p>positive {posPercent}%</p>
     </div>
   )
 }
